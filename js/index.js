7b8a76205a34c5fc983fac604ba6f1d94306e03f
Jeffery <yeekfeitan@triaset.com>
2014-08-15 17:55:18 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 //Variables
 var quickSearch = "title";
 
@@ -6,45 +7,45 @@ var quickSearch = "title";
 $(window).on("navigate", function (event, data) {
   var direction = data.state.direction;
   if (direction == 'back') {
-    
+
     $.mobile.changePage($(document.location.href = "#"+$.mobile.activePage.attr("id")), {transition: 'none'});
     event.preventDefault();
-  }
-  if (direction == 'forward') {
+}
+if (direction == 'forward') {
     // do something else
-  }
+}
 });
 
 //DOM
 $( document ).on( "pageinit", "#search", function() {
-        $( "#autocomplete" ).on( "filterablebeforefilter", function ( e, data ) {
-            var $ul = $( this ),
-            $input = $( data.input ),
-            value = $input.val(),
-            html = "";
-            $ul.html( "" );
-            if ( value && value.length > 0 ) {
-                console.log("quickSearch: " + quickSearch);
-                $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-                $ul.listview( "refresh" );
-                $.ajax({
-                    url: "http://gd.geobytes.com/AutoCompleteCity",
-                    dataType: "jsonp",
-                    crossDomain: true,
-                    data: {
-                        q: $input.val()
-                    }
-                })
-                .then( function ( response ) {
-                    $.each( response, function ( i, val ) {
-                        html += "<li>" + val + "</li>";
-                    });
-                    $ul.html( html );
-                    $ul.listview( "refresh" );
-                    $ul.trigger( "updatelayout");
+    $( "#autocomplete" ).on( "filterablebeforefilter", function ( e, data ) {
+        var $ul = $( this ),
+        $input = $( data.input ),
+        value = $input.val(),
+        html = "";
+        $ul.html( "" );
+        if ( value && value.length > 0 ) {
+            console.log("quickSearch: " + quickSearch);
+            $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
+            $ul.listview( "refresh" );
+            $.ajax({
+                url: "http://gd.geobytes.com/AutoCompleteCity",
+                dataType: "jsonp",
+                crossDomain: true,
+                data: {
+                    q: $input.val()
+                }
+            })
+            .then( function ( response ) {
+                $.each( response, function ( i, val ) {
+                    html += "<li>" + val + "</li>";
                 });
-            }
-        });
+                $ul.html( html );
+                $ul.listview( "refresh" );
+                $ul.trigger( "updatelayout");
+            });
+        }
+    });
 });
 
 
@@ -75,3 +76,75 @@ $( document ).on( "pageinit", "#mainpage", function() {
 });
 
 
+// $(document).on("pageinit", function(){
+//     var serviceURL = "../php/services.php"; 
+//     var getNumber = function() {
+//         return $.ajax({
+//             type: "post",
+//             beforeSend: function() { $.mobile.loading( 'show' ) }, //Show spinner
+//             complete: function() { $.mobile.loading( 'hide' ) }, //Hide spinner
+//             async: "true",
+//             dataType: 'json',
+//             url: serviceURL,
+//             data: { listomatic: $.mobile.listomatic.prototype.getResults() },
+//             success: function(data) {
+//                 if (data && data.numbers) {
+//                     var list = '';
+//                     $.each(data.numbers, function(index, value) {
+//                         list += '<li data-icon="false" data-filtertext="' + value.date + '">' +
+//                                 '<div class="latestNumber">' +
+//                                 '<span class="circle">' + value.number[1] + '</span>' +
+//                                 '<span class="circle">' + value.number[2] + '</span>' +
+//                                 '<span class="circle">' + value.number[3] + '</span>' +
+//                                 '<span class="circle">' + value.number[4] + '</span>' +
+//                                 '<span class="circle">' + value.number[5] + '</span>' +
+//                                 '<span class="circle powerball">' + value.number[6] + '</span>' +
+//                                 '</div>' +
+//                                 '<p style="text-align:center;" class="latestNumberDate">' + value.date + '</p>' +
+//                                 '</li>';
+//                     }); // end each 
+//                     $('#listview').append(list).listview("refresh");
+//                 } // end if
+//             } // end success
+//         }); // end ajax
+//     }
+//     $.extend($.mobile.listomatic.prototype.options, {perPage: 2, btnLabel: 'Show Me More', refreshContent: 'daily'});
+//     $.mobile.listomatic.prototype.registerAjaxCall(getNumber);
+//     console.log("Tested");
+// });
+
+
+//Test for iscrollview
+$(document).delegate("#search2", "pageinit", function(event){
+
+
+    $(".iscroll-wrapper", this).bind({
+        iscroll_onpulldown: function(event, data){
+            //react to a pull-down as necessary here
+            console.log("retrieving / refreshing.. ");
+            //then call this to reset the pull-to-refresh controls:
+            data.iscrollview.refresh();
+        },
+
+        iscroll_onpullup: function(event, data){
+            //load more history
+            console.log("loading more content..");
+            //reset the pull-up-to-load-more controls
+            data.iscrollview.refresh();
+        },
+        iscroll_onscrollend: function(event, data){
+            if($(document).height() > $(window).height())
+            {
+                if($(window).scrollTop() == $(document).height() - $(window).height()){
+                    console.log("The Bottom");
+                }
+            }
+            
+        }
+    }
+
+
+
+
+    );
+});
--- php/services.php
+++ php/services.php
@@ -1,16 +1,63 @@
 <?php
-
+error_reporting(0);
+header('Access-Control-Allow-Origin: *');
+header( 'Content-Type: text/html; charset=utf-8' ); 
 function __autoload($class_name) {
 	include_once 'inc/' . $class_name . '.php';
 }
 
 //Get the db config file from global external source
-	$file = '../conf/db.ini'; 
-	session_start();
+$file = '../conf/db.ini'; 
+session_start();
 
-    if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
+if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
 
-    
+$user = $settings['database']['username'];
+$pswd =  $settings['database']['password'];
+$db = $settings['database']['schema'];
+$conn = mysql_connect( $settings['database']['host'], $user, $pswd);  
+mysql_set_charset('utf8',$conn); 
+mysql_select_db($db, $conn);
+
+$perPage    = $_REQUEST['listomatic']['perPage'];
+$listOffset = $_REQUEST['listomatic']['listOffset'];
+$searchTerm = $_REQUEST['listomatic']['searchTerm'];
+
+if ($searchTerm) {
+	$sql = "SELECT SQL_CALC_FOUND_ROWS *
+	FROM audio
+	WHERE audio_title LIKE '%$searchTerm%'
+	ORDER BY audio_added_date DESC
+	LIMIT $listOffset, $perPage";
+} else {
+	$sql = "SELECT SQL_CALC_FOUND_ROWS *
+	FROM audio_title
+	ORDER BY audio_added_date DESC
+	LIMIT $listOffset, $perPage";
+}
+$result = mysqli_query($con, $sql);
+
+// If you are using MySQL use SQL_CALC_FOUND_ROWS in your main queries (above)
+// Now to get the total records available use the FOUND_ROWS() function (below)
+$resultNumRows = mysqli_query($con, 'SELECT FOUND_ROWS() as foundRows');
+$rowFoundRows = mysqli_fetch_array($resultNumRows);
+$iFoundRows = $rowFoundRows['foundRows'];
+
+while($row = mysqli_fetch_array($result)) {
+	$sDate = date('m/d/Y', strtotime($row['date']));
+    // Listomatic requires the "total" field to show/hide the "Show More" button
+	$aData['total'] = $iFoundRows; 
+    // The following is sample data (in this case Powerball numbers) that you want to display
+	$aData['numbers'][] = array('date' =>  $sDate,
+		'number' => array(1 => $row['num1'],
+			2 => $row['num2'],
+			3 => $row['num3'],
+			4 => $row['num4'],
+			5 => $row['num5'],
+			6 => $row['num6']));
+}
+echo json_encode($aData);
+exit;
 
 
 ?>
